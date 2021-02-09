61e05b08f757feccfa551bd0aa5566bcf6a85c24
Melusine Tenkoff <600022693@fjuhsd.org>
2021-02-09 21:30:17 +0000

    Number class and main

--- Number.java
+++ Number.java
@@ -1,7 +1,7 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-
+//Name - Mel Tenkoff
+import java.util.ArrayList;
 
 //write the Number class
 //use the handout and
@@ -9,20 +9,68 @@
 //to guide you
 
 //header for Number class
-
-//private instance variable (must be an Integer)
-
-//constructor
-
-
-//getter method
-
-
-//setter method
-
-//isOdd method
-
-//isPerfect method
-
-
-//toString
+public class Number
+{
+  //private instance variable (must be an Integer)
+  private int num;  
+  //constructor
+  public Number(int initNum)
+  {
+    num = initNum;
+  }
+  //getter method
+  public int getNum()
+  {
+    return num;
+  }
+  //setter method
+  public void setNum(int newNum)
+  {
+    num = newNum;
+  }
+  //isOdd method
+  //first method header for isOdd, returns a boolean
+  public boolean isOdd()
+  {
+    //if statement - if the number%2 = 1
+    if(num % 2 == 1)
+    {
+      //return true
+      return true;
+    }
+    //return false
+    return false;
+  }
+  //isPerfect method
+  //second method header for isPerfect, return a boolean
+  public boolean isPerfect()
+  {
+    //create an array list containing ints named perfect
+    ArrayList<Integer> perfect = new ArrayList<Integer>();
+    //make an int called sum
+    int sum = 0;
+    //use a for loop - set the control var to 1; while the control var is less than the number; increment the control var
+    for(int i = 1; i < num; i++)
+    {
+      //if statement - if the number%control var = 0
+      if(num % i == 0)
+      {
+        //add the control var to odd to sum
+        sum += i;
+      }
+    }
+    //if statement - if the number = sum
+    if(num == sum)
+    {
+      //return true
+      return true;
+    }
+    //return false
+    return false;
+  }
+  //toString
+  public String toString()
+  {
+    return ("number: " + num);
+  }
+}
